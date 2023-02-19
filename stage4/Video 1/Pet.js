1735630551626d298821ca0b8edb1cb4a5f80d73
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-19 17:06:45 -0600

    Unit 4 Ojbect Oriented JavaScript course video 1 in 3rd section complete

--- stage4/Video 1/Pet.js
+++ stage4/Video 1/Pet.js
@@ -5,6 +5,18 @@ class Pet {
     this.breed = breed;
     this.sound = sound;
   }
+
+  get activity(){
+    const today = new Date();
+    const hour = today.getHours();
+
+    if (hour > 8 && hour <= 20){
+      return 'playing';
+    } else {
+      return 'sleeping';
+    }
+
+  }
   
   speak() {
     console.log(this.sound);
@@ -15,7 +27,8 @@ class Pet {
 const ernie = new Pet('dog', 1, 'pug', 'yip yip');
 const vera = new Pet('dog', 8, 'border collie', 'woof woof');
 
-
+console.log(ernie.activity);
+console.log(ernie);
 
 
 
