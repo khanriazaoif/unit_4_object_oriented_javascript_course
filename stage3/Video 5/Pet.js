520fb320ae7a83571774b6a9cc0a7ee0cae4be7c
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-19 16:50:34 -0600

    Unit 4 Ojbect Oriented JavaScript course video 5 complete

--- stage3/Video 5/Pet.js
+++ stage3/Video 5/Pet.js
@@ -1,10 +1,20 @@
 class Pet {
-  constructor(animal, age, breed) {
+  constructor(animal, age, breed, sound) {
     this.animal = animal;
     this.age = age;
     this.breed = breed;
+    this.sound = sound;
   }
+
+  speak(){
+    console.log(this.sound);
+  }
+
 }
 
-const ernie = new Pet('dog', 1, 'pug');
-const vera = new Pet('dog', 8, 'border collie');
+
+const ernie = new Pet('dog', 1, 'pug','yip yip');
+const vera = new Pet('dog', 8, 'border collie', 'woof woof');
+
+ernie.speak();
+vera.speak();
